e18b36d53a0ba1f01bbf9cb9e732d31a96dc075e
giakiet1214 <giakietlehoang@gmail.com>
2025-10-16 00:01:55 +0700

    hoan thanh giao dien

--- resources/views/userViews/pages/log-in.blade.php
+++ resources/views/userViews/pages/log-in.blade.php
@@ -3,6 +3,18 @@
 @section('title','Log in')
 
 @section('content')
-    day la trang login
+    {{-- day la trang login --}}
 <!--cần xây dựng giao diện và form,chức năng đăng nhập (15/10/2025 trongphuc)-->
+<div class="login">
+	<div class="container">
+		<h3>LOG-IN</h3>
+		<p>Please sign up if you do have an account.</p>
+		<form>
+            {{-- đăng kí tên user email mat khau nhap lại mật khẩu --}}
+			 <input class="user" type="text" placeholder="USER NAME" required=""><br>
+			 <input class="user" type="password" placeholder="PASSWORD" required="">
+			 <input type="submit" value="SEND">
+		</form>
+	</div>
+</div>
 @endsection
